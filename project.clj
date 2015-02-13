b0db0ac33f1925fe2f1eb3b25248d474ca571506
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-02-13 10:24:53 +0100

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.spootnik/watchman "0.3.1"
+(defproject org.spootnik/watchman "0.3.2"
   :description "watch directory for changes"
   :url "https://github.com/pyr/watchman"
   :license {:name "MIT License"
