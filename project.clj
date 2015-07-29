9accb0b098455be24bcf17fe60f15dedcf6ad2be
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-07-29 15:48:05 +0200

    project: clojure 1.7 is out now

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject spootnik/watchman "0.3.4"
+(defproject spootnik/watchman "0.3.5"
   :description "Watch directories for changes"
   :url "https://github.com/pyr/watchman"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure    "1.7.0-RC.1"]
+  :dependencies [[org.clojure/clojure    "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]])
