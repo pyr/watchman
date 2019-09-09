89ab2345256b26ae13491f27d4168b8274f81964
Pierre-Yves Ritschard <pyr@spootnik.org>
2019-09-09 13:22:36 +0200

    project: improvements

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject spootnik/watchman "0.3.7"
+(defproject spootnik/watchman "0.3.8-SNAPSHOT"
   :description "Watch directories for changes"
   :url "https://github.com/pyr/watchman"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :global-vars {*warn-on-reflection* true}
   :plugins [[lein-ancient "0.6.15"]]
-  :dependencies [[org.clojure/clojure "1.10.0"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]])
